43590ff7887f044d884b3467d793472d18930231
tiakelly02 <72085203+tiakelly02@users.noreply.github.com>
2020-11-28 19:26:18 +0000

    shooter

---
+++ GalaxyShooter/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//This is script is for the players movement 
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public GameObject projectile;
+    public GameObject projectileClone;
+    public GameObject player;
+ 
+    void Update()
+    {
+        ArrowKeys();
+        FireBullet();
+    }
+
+        void ArrowKeys()
+        {
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(new Vector3(4 * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(new Vector3(-4 * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(new Vector3(0,4 * Time.deltaTime,0));
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(new Vector3(0, -4 * Time.deltaTime, 0));
+        }
+     }
+
+    void FireBullet()
+     {
+          if (Input.GetKey(KeyCode.Space)) { 
+           projectileClone = Instantiate(projectile, new Vector3(player.transform.position.x,player.transform.position.y +0.7f,0),player.transform.rotation) as GameObject; 
+        }
+    }
+}
---
+++ GalaxyShooter/Assets/Scripts/Projectile.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//This script is for the projectile/bullets to move 
+public class Projectile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(0,3 * Time.deltaTime, 0));
+    }
+}
