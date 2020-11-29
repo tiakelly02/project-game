62a881d116dde64ece0a8ec53f88ec5a4126e6b3
tiakelly02 <72085203+tiakelly02@users.noreply.github.com>
2020-11-29 22:23:32 +0000

    ..

---
+++ GalaxyShooter/Assets/Scripts/Timer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Timer : MonoBehaviour
+{
+    float currentTime = 0f;
+    float startingTime = 10f;
+
+    public Text countdownText;
+
+    void Start() {
+        currentTime = startingTime;
+    }
+    private void Update(){
+        currentTime -= 1 * Time.deltaTime;
+        countdownText.text = currentTime.ToString("0");
+
+        if (currentTime <= 0) {
+            currentTime = 0;
+        }
+    }
+}
